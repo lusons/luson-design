c28ad93cc73d8241c8e99e4ebcf02aa5eddd9e41
lusons <2296104253@qq.com>
2022-06-05 18:20:28 +0800

    feat: add storybook

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentMeta } from "@storybook/react";
+import { Button } from ".";
+import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
+
+export const ButtonTest = () => {
+  return (
+    <>
+      <Button
+        type={"primary"}
+        size='middle'
+        onClick={action('点我')}
+      >
+        点我
+      </Button>
+      <Button type={"danger"} size="small">
+        点我
+      </Button>
+      <Button type={"default"}>default</Button>
+      <Button type={"default"} disabled>
+        disabled
+      </Button>
+      <Button type={"link"} href="https://www.baidu.com" >
+        link
+      </Button>
+    </>
+  );
+};
+
+export default {
+  title: 'Luson/Button',
+  component: ButtonTest,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (storyRender) => <div style={{ textAlign: 'center' }}>{storyRender()}</div>,
+    withInfo
+  ]
+} as ComponentMeta<typeof ButtonTest>;
